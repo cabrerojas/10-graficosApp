913ab3b77677c0d086a7fb8424c4b18ce12f351b
cabrerojas <gcabrerojas@gmail.com>
2021-05-03 18:49:22 -0400

    rutas
    Se añaden las rutas y se implementa un LazyLoad para el modulo de gráficos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'graficos',
+    loadChildren: () => import ('./graficos/graficos.module').then(m => m.GraficosModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'graficos'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/graficos/graficos-routing.module.ts
+++ src/app/graficos/graficos-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BarrasDobleComponent } from './pages/barras-doble/barras-doble.component';
+import { BarrasComponent } from './pages/barras/barras.component';
+import { DonaComponent } from './pages/dona/dona.component';
+import { DonaHttpComponent } from './pages/dona-http/dona-http.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: 'barra', component: BarrasComponent },
+      { path: 'barras-dobles', component: BarrasDobleComponent },
+      { path: 'dona', component: DonaComponent },
+      { path: 'dona-http', component: DonaHttpComponent },
+      { path: '**', redirectTo: 'barra' },
+    ]
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
