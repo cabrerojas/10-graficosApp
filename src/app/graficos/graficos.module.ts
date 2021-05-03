e66590bd4070db87dadddacec69c3662c4247a31
cabrerojas <gcabrerojas@gmail.com>
2021-05-03 18:29:46 -0400

    estructura del proyecto
    se definen los módulos con sus respectivos componentes

---
+++ src/app/graficos/graficos.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GraficosRoutingModule } from './graficos-routing.module';
+import { BarrasComponent } from './pages/barras/barras.component';
+import { BarrasDobleComponent } from './pages/barras-doble/barras-doble.component';
+import { DonaComponent } from './pages/dona/dona.component';
+import { DonaHttpComponent } from './pages/dona-http/dona-http.component';
+import { GraficoBarraComponent } from './components/grafico-barra/grafico-barra.component';
+
+
+@NgModule({
+  declarations: [
+    BarrasComponent,
+    BarrasDobleComponent,
+    DonaComponent,
+    DonaHttpComponent,
+    GraficoBarraComponent
+  ],
+  imports: [
+    CommonModule,
+    GraficosRoutingModule
+  ]
+})
+export class GraficosModule { }
