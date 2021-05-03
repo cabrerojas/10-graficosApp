cb0dea60dbf2eeb2de9a033d969a3f40be5afd1f
cabrerojas <gcabrerojas@gmail.com>
2021-05-03 19:03:11 -0400

    menú

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+
+interface MenuItem {
+  ruta: string;
+  texto: string;
+}
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
+
+  menuItem: MenuItem[] = [
+    {
+      ruta: '/graficos/barra',
+      texto: 'Barra'
+    },
+    {
+      ruta: '/graficos/barras-dobles',
+      texto: 'Barras Dobles'
+    },
+    {
+      ruta: '/graficos/dona',
+      texto: 'Dona'
+    },
+    {
+      ruta: '/graficos/dona-http',
+      texto: 'Dona Http'
+    },
+  ];
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
 
 }
