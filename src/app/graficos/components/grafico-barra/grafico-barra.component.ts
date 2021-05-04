e1ca3a23a084f61e0340f843159748e3010d99ba
cabrerojas <gcabrerojas@gmail.com>
2021-05-03 20:27:29 -0400

    gráficos dobles
    se utiliza el componente del gráfico añadiéndole inputs para recibir la data del componente padre

--- src/app/graficos/components/grafico-barra/grafico-barra.component.ts
+++ src/app/graficos/components/grafico-barra/grafico-barra.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
 import { Label } from 'ng2-charts';
 
@@ -9,54 +9,34 @@ import { Label } from 'ng2-charts';
 })
 export class GraficoBarraComponent implements OnInit {
 
+  @Input() horizontal = false;
+
+  @Input() barChartLabels: Label[] = [
+    // '2015', '2016', '2017', '2018', '2019', '2020', '2021'
+  ];
+  @Input() barChartData: ChartDataSets[] = [
+    // { data: [65, 59, 80, 81, 56, 55, 40], label: 'Enero', backgroundColor: '#BDC2F2', hoverBackgroundColor: 'grey'  },
+    // { data: [28, 48, 40, 19, 86, 27, 90], label: 'Febrero', backgroundColor: '#B5C3DB', hoverBackgroundColor: 'grey' },
+    // { data: [8, 38, 80, 89, 16, 57, 60], label: 'Marzo', backgroundColor: '#BDDFF2', hoverBackgroundColor: 'grey' }
+  ];
+
   public barChartOptions: ChartOptions = {
     responsive: true,
   };
-  public barChartLabels: Label[] = ['2015', '2016', '2017', '2018', '2019', '2020', '2021'];
+
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
 
-  public barChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Enero', backgroundColor: '#BDC2F2', hoverBackgroundColor: 'grey'  },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Febrero', backgroundColor: '#B5C3DB', hoverBackgroundColor: 'grey' },
-    { data: [8, 38, 80, 89, 16, 57, 60], label: 'Marzo', backgroundColor: '#BDDFF2', hoverBackgroundColor: 'grey' }
-  ];
 
 
   constructor() { }
 
   ngOnInit(): void {
-  }
 
-  public randomize(): void {
-    this.barChartData[0].data = [
-      Math.round( Math.random() * 100 ),
-      Math.round( Math.random() * 100 ),
-      Math.round( Math.random() * 100 ),
-      Math.round( Math.random() * 100 ),
-      Math.round( Math.random() * 100 ),
-      Math.round( Math.random() * 100 ),
-      Math.round( Math.random() * 100 ),
-    ];
-    this.barChartData[1].data = [
-      Math.round( Math.random() * 100 ),
-      Math.round( Math.random() * 100 ),
-      Math.round( Math.random() * 100 ),
-      Math.round( Math.random() * 100 ),
-      Math.round( Math.random() * 100 ),
-      Math.round( Math.random() * 100 ),
-      Math.round( Math.random() * 100 ),
-    ];
-    this.barChartData[2].data = [
-      Math.round( Math.random() * 100 ),
-      Math.round( Math.random() * 100 ),
-      Math.round( Math.random() * 100 ),
-      Math.round( Math.random() * 100 ),
-      Math.round( Math.random() * 100 ),
-      Math.round( Math.random() * 100 ),
-      Math.round( Math.random() * 100 ),
-    ];
-  }
+    if (this.horizontal) {
+      this.barChartType = 'horizontalBar';
+    }
 
+  }
 
 }
