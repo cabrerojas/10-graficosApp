9cfd1c5f430d5a89c15562d23f7706d215d8b337
cabrerojas <gcabrerojas@gmail.com>
2021-05-03 21:04:30 -0400

    dona http
    Se consume api con los datos de las rrss y se muestra en un gráfico de donas

--- src/app/graficos/pages/dona-http/dona-http.component.ts
+++ src/app/graficos/pages/dona-http/dona-http.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { Color, Label, MultiDataSet } from 'ng2-charts';
 import { GraficosService } from '../../services/graficos.service';
 
 @Component({
@@ -8,13 +10,38 @@ import { GraficosService } from '../../services/graficos.service';
 })
 export class DonaHttpComponent implements OnInit {
 
+
+  // Doughnut
+  public doughnutChartLabels: Label[] = [
+    // 'Download Sales', 'In-Store Sales', 'Mail-Order Sales', 'Others'
+    ];
+  public doughnutChartData: MultiDataSet = [
+    // [350, 450, 100, 269],
+  ];
+  public doughnutChartType: ChartType = 'doughnut';
+
+  public colors: Color[] = [
+   {
+     backgroundColor: [
+       '#A0EB92',
+       '#FFB870',
+       '#B475EB',
+     ]
+   }
+ ];
+
   constructor(private graficosService: GraficosService) { }
 
   ngOnInit(): void {
 
     this.graficosService.getUsuariosRRSS()
           .subscribe( data => {
-            console.log(data); 
+            const labels = Object.keys( data );
+            const values = Object.values( data );
+
+            this.doughnutChartLabels = labels;
+            this.doughnutChartData.push(values);
+
           } );
   }
 
